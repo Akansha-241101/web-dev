a967ea41c432a13e8a8859caec037e2c5901991e
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-13 03:27:24 +0530

    java script concepts revised

--- day-8/script.js
+++ day-8/script.js
@@ -1,34 +1,124 @@
+// ========================================
+// 1. IMPORT / EXPORT
+// ========================================
+
 // import { name, age } from "./variables.js";
 
 // const greet = () => {
 //   console.log(`Hi ${name} ${age}`);
 // };
+
 // greet();
 
-// objects
+
+// ========================================
+// 2. OBJECTS
+// ========================================
 
 // let personOne = {
 //   name: "Akansha",
 //   age: 24,
 // };
 
+// // Mutating an object
 // console.log(personOne.age = 23);
 
-// spread operator
+
+// ========================================
+// 3. SPREAD OPERATOR
+// ========================================
 
 // let person = {
 //   name: "Akansha",
 //   age: 24,
 // };
 
-// let personCopy = {...person}
+// // Creating a copy of an object
+// let personCopy = { ...person };
 
 // console.log(personCopy);
 
-// Array & array methods
+
+// ========================================
+// 4. ARRAYS & ARRAY METHODS
+// ========================================
 
 // const numbers = [6, 2, 10, 7];
 
+// // filter()
 // const filteredNumbers = numbers.filter((number) => number > 5);
 
 // console.log(filteredNumbers);
+
+
+// ========================================
+// 5. MAP()
+// ========================================
+
+// const numbers = [2, 3, 5];
+
+// const multipliedByTwo = numbers.map((number) => number * 2);
+
+// console.log(multipliedByTwo);
+
+
+// ========================================
+// 6. OBJECT DESTRUCTURING
+// ========================================
+
+// const person = {
+//   name: "Akansha",
+//   age: 24,
+// };
+
+// // Without destructuring
+// const name = person.name;
+// const age = person.age;
+
+// // With destructuring
+// const { name, age } = person;
+
+// console.log(name);
+
+
+// ========================================
+// 7. IF / ELSE
+// ========================================
+
+// let age = 24;
+
+// if (age > 18) {
+//   console.log("eligible for driving");
+// } else {
+//   console.log("not eligible for driving");
+// }
+
+
+// ========================================
+// 8. TERNARY OPERATOR
+// ========================================
+
+// let age = 24;
+
+// age > 18
+//   ? console.log("eligible for driving")
+//   : console.log("not eligible for driving");
+
+
+// ========================================
+// 9. IF / ELSE INSIDE A FUNCTION
+// ========================================
+
+// let age = 24;
+
+// function checkEligibility() {
+//   if (age > 18) {
+//     return "eligible for driving";
+//   } else {
+//     return "not eligible for driving";
+//   }
+// }
+
+// const result = checkEligibility();
+
+// console.log(result);
