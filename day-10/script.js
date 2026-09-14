cf2091f8e33a37b0be873b23a8a584888a4cafdf
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-14 14:53:29 +0530

    Add initial script.js file with examples for functions, objects, spread operator, and array methods

---
+++ day-10/script.js
@@ -0,0 +1,53 @@
+// variable
+// console.log();
+// function
+
+// example for functions.
+
+// const calculatePrice = (price, quantity) => {
+//   return price * quantity;
+// };
+
+// const totalPrice = calculatePrice(18, 2);
+
+// const checkFreeDelivery = () => {
+//   if (totalPrice > 30) {
+//     console.log("Free delivery available");
+//   } else {
+//     console.log("Free delivery is not available")
+//   }
+// }
+
+// checkFreeDelivery();
+
+// example for objects
+
+// let person = {
+//   name: "Akansha",
+//   age: 24,
+//   height: "4ft",
+// };
+
+// console.log(person.age);
+
+// example of spread operator
+
+// let car = {
+//   brand: "Toyota",
+//   color: "Red",
+//   speed: 80,
+//   isRunning: true,
+// };
+
+// let carCopy = { ...car };
+
+// console.log(carCopy);
+
+// example for array
+
+// const persons = ["chinky", "minky", "pinky", "monkey"];
+
+// example of array methods
+
+// const filteredPersons = persons.filter((person) => person[0] === "m");
+// console.log(filteredPersons);
