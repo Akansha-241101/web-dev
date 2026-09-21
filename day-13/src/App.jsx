3dc680573ceb10c6d8186b6b250db9373b78b18f
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-21 20:32:39 +0530

    refactor: restructure App component and add Button and Card components

--- day-13/src/App.jsx
+++ day-13/src/App.jsx
@@ -1,5 +1,57 @@
-function App() {
-  return <body className="h-full bg-purple-200 text-7xl text-black flex justify-center-safe align-middle">Assalamwalikum</body>;
+function Button({ text }) {
+  return (
+    <button className="bg-purple-600 text-white px-3 py-2 rounded-md">
+      {text}
+    </button>
+  );
 }
 
+// function Button(props) {
+//   return (
+//     <button className="bg-purple-600 text-white px-3 py-2 rounded-md">
+//       {props.text}
+//     </button>
+//   );
+// }
+
+function Card(props) {
+  return (
+    <article className="bg-gray-200 text-black p-4 flex flex-col gap-2 border border-gray-400">
+      <p>{props.imageUrl}</p>
+      <p>{props.title}</p>
+      <p>{props.price}</p>
+      <p>{props.rating}</p>
+      <p>{props.quantity}</p>
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="bg-purple-200 text-7xl text-black flex justify-center items-center">
+        Assalamwalikum
+      </h1>
+      <div className="flex gap-3">
+        <Button text="Get Started" />
+        <Button text="Explore Features" />
+        <Button text="akdfbadub" />
+        <Card
+          imageUrl="./images/image-1.png"
+          title="Card 1"
+          price={20}
+          rating="4.00"
+          quantity={3}
+        />
+        <Card
+          imageUrl="./images/image-2.png"
+          title="Card 2"
+          price={30}
+          rating="5.00"
+          quantity={1}
+        />
+      </div>
+    </div>
+  );
+}
 export default App;
