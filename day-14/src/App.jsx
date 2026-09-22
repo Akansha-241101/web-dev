35fa4a6a8257a65485e9bcee5a15001eb2a4b373
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-22 19:47:57 +0530

    refactor: clean up App component and update image imports in contentDetail.js

--- day-14/src/App.jsx
+++ day-14/src/App.jsx
@@ -1,44 +1,11 @@
-// 1. import images
-import imageOne from "./images/image-1.jpg";
-import imageTwo from "./images/image-2.jpg";
-import imageThree from "./images/image-3.jpg";
+// imports here
 
-// 2. create the ImageComponent
-
-function ImageComponent({ image, title, description }) {
-  return (
-    <div className="flex item-center gap-4">
-      <img className="w-100" src={image} alt={title} />
-      <div className="flex flex-col justify-center gap-2">
-        <p className="text-text text-4xl font-semibold">{title}</p>
-        <p className="text-text text-xl">{description}</p>
-      </div>
-    </div>
-  );
-}
+// create components here
 
 function App() {
   return (
-    <main className="min-h-screen flex flex-col gap-4 items-center bg-background text-text p-10">
-      {
-        /* Call the ImageComponent with props */ <>
-          <ImageComponent
-            image={imageOne}
-            title="Image 1"
-            description="This is image 1"
-          />
-          <ImageComponent
-            image={imageTwo}
-            title="Image 2"
-            description="This is image 2"
-          />
-          <ImageComponent
-            image={imageThree}
-            title="Image 3"
-            description="This is image 3"
-          />
-        </>
-      }
+    <main>
+      {/* call the components here */}
     </main>
   );
 }
--- day-14/src/Content/contentDetail.js
+++ day-14/src/Content/contentDetail.js
@@ -1,39 +1,48 @@
+import pet1 from "../images/pet-1.jpg";
+import pet2 from "../images/pet-2.jpg";
+import pet3 from "../images/pet-3.jpg";
+import pet4 from "../images/pet-4.jpg";
+import pet5 from "../images/pet-5.jpg";
 
-
+import book1 from "../images/book-1.jpg";
+import book2 from "../images/book-2.jpg";
+import book3 from "../images/book-3.jpg";
+import book4 from "../images/book-4.jpg";
+import book5 from "../images/book-5.jpg";
 
 //example 1 PET ADOPTION CARDS array//
 
 const pets = [
   {
-    image: "pet-1.jpg",
+    image: pet1,
     name: "Bruno",
     type: "Dog",
     age: "2 years",
     location: "Mumbai",
   },
   {
-    image: "pet-2.jpg",
+    image: pet2,
     name: "Luna",
     type: "Cat",
     age: "1 year",
     location: "Delhi",
   },
   {
-    image: "pet-3.jpg",
+    image: pet3,
     name: "Coco",
     type: "Parrot",
     age: "8 months",
     location: "Bengaluru",
   },
   {
-    image: "pet-4.jpg",
+    image: pet4,
     name: "Milo",
     type: "Rabbit",
     age: "1.5 years",
     location: "Pune",
   },
   {
-    image: "pet-5.jpg",
+    image: pet5,
     name: "Bella",
     type: "Dog",
     age: "3 years",
@@ -41,40 +50,39 @@ const pets = [
   },
 ];
 
-
 //example 2 BOOK CARDS array//
 
 const books = [
   {
-    image: "book-1.jpg",
+    image: book1,
     title: "Atomic Habits",
     author: "James Clear",
     price: 499,
     category: "Self Help",
   },
   {
-    image: "book-2.jpg",
+    image: book2,
     title: "The Alchemist",
     author: "Paulo Coelho",
     price: 350,
     category: "Fiction",
   },
   {
-    image: "book-3.jpg",
+    image: book3,
     title: "Rich Dad Poor Dad",
     author: "Robert T. Kiyosaki",
     price: 399,
     category: "Finance",
   },
   {
-    image: "book-4.jpg",
+    image: book4,
     title: "Ikigai",
     author: "Héctor García",
     price: 299,
     category: "Personal Growth",
   },
   {
-    image: "book-5.jpg",
+    image: book5,
     title: "Deep Work",
     author: "Cal Newport",
     price: 450,
