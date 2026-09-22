1530a656a4de50e24cfb5668eae22760430589b4
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-22 11:44:32 +0530

    React Task created day-14

---
+++ day-14/src/App.jsx
@@ -0,0 +1,13 @@
+// 1. import images
+
+// 2. create the ImageComponent
+
+function App() {
+  return (
+    <main className="min-h-screen flex flex-col gap-4 items-center bg-background text-text p-10">
+      {/* Call the ImageComponent with props */}
+    </main>
+  );
+}
+
+export default App;
