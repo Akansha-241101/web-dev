1de9edc06d7a6b73862b4347d08320640da23cba
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-09-23 11:09:43 +0530

    feat: add MenuIcon component and implement count increment/reset functionality in App

--- day-15/src/App.jsx
+++ day-15/src/App.jsx
@@ -1,40 +1,51 @@
 import { useState } from "react";
-
-function MenuIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-menu icon menu"
-    >
-      <path d="M4 5h16" />
-      <path d="M4 12h16" />
-      <path d="M4 19h16" />
-    </svg>
-  );
-}
+import MenuIcon from "./components/MenuIcon";
 
 function App() {
+
   // const [var, func] = useState();
+  const [count, setCount] = useState(0);
   const [showMenu, setShowMenu] = useState(false);
+
   return (
     <main className="p-6">
       <div className="flex flex-col gap-3">
+
+        {/* -------------------- count increment and reset using useState ---------------- */}
+
+        {/* Count display */}
+        <h1 className="text-3xl">{count}</h1>
+
+        {/* Buttons */}
+        <div className="flex gap-3">
+          <button
+            className="w-fit bg-gray-200 border py-2 px-4 cursor-pointer"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Inc count
+          </button>
+          <button
+            className="w-fit bg-gray-200 border py-2 px-4 cursor-pointer"
+            onClick={() => setCount(0)}
+          >
+            reset count
+          </button>
+        </div>
+
+        {/* -------------------- Menu show and hide using useState ---------------- */}
+
+        {/* Menu toggle button */}
         <button
-          className="w-fit bg-gray-200 border py-2 px-4"
+          className="w-fit bg-gray-200 border py-2 px-4 cursor-pointer"
           onClick={() => {
             setShowMenu((prev) => !prev);
           }}
         >
+          {/*  Menu SVG icon component */}
           <MenuIcon />
         </button>
+
+        {/* Conditionally display the menu */}
         {showMenu ? (
           <div className="w-100 flex flex-col bg-gray-200 border border-text divide-y">
             <a href="" className="p-3">
