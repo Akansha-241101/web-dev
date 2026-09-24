ac9336b23eda53c2f9a9e92b6c02067c6aee9704
Vicky6576 <vickramade@gmail.com>
2026-09-24 23:10:25 +0530

    refactor: reorganize Header component buttons and integrate MenuIcon for better functionality

--- day-12/src/components/shared/Header.jsx
+++ day-12/src/components/shared/Header.jsx
@@ -28,22 +28,43 @@ function Header() {
       <button className="bg-primary py-2 px-6 text-primary-text rounded-md hidden lg:block">
         {headerContent.button}
       </button>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-menu lg:hidden"
-      >
-        <path d="M4 5h16" />
-        <path d="M4 12h16" />
-        <path d="M4 19h16" />
-      </svg>
+      <button className="w-fit bg-black-700">
+
+
+
+
+// <button
+          className="w-fit bg-gray-200 border py-2 px-4 cursor-pointer"
+          onClick={() => {
+            setShowMenu((prev) => !prev);
+          }}
+        >
+          {/*  Menu SVG icon component */}
+          <MenuIcon />
+        </button>
+
+
+
+
+
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-menu lg:hidden"
+        >
+          <path d="M4 5h16" />
+          <path d="M4 12h16" />
+          <path d="M4 19h16" />
+        </svg>
+      </button>
     </header>
   );
 }
